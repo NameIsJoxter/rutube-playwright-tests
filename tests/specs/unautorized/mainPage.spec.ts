03b8c21a0aaec4f06124f742926fe1c55efe1681
Galina <quellik@gmail.com.com>
2025-06-14 20:27:34 +0300

    add main page fixture

--- tests/specs/unautorized/mainPage.spec.ts
+++ tests/specs/unautorized/mainPage.spec.ts
@@ -1,31 +1,14 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
-test('Открытие главной страницы', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-});
-
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.clozeCookieAlert();
-  await mainPage.clozeAdvertisingBanner();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
 
-test('Проверка доступности табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.clozeCookieAlert();
-  await mainPage.clozeAdvertisingBanner();
+test('Проверка доступности табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabHasCorrectAriaSnapshot();
 });
 
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.clozeCookieAlert();
-  await mainPage.clozeAdvertisingBanner();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });

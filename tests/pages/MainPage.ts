edd9bc0764ba8209dbe08c56b6b753d87b17e5f0
Galina <quellik@gmail.com.com>
2025-06-14 20:14:51 +0300

    add main page tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,8 +1,35 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class MainPage extends BasePage {
+  private readonly headerLocator: Locator;
+  private readonly categoriesTabLocator: Locator;
+  private readonly menuLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.headerLocator = this.page.locator('.header-module__mainHeader');
+    this.categoriesTabLocator = this.page.locator(
+      '.freyja_pen-tabs-snipped-list__snipped-list_IgInW',
+    );
+    this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+  }
   async open() {
     await this.page.goto('https://rutube.ru/');
+  }
+  async headerHasCorrectAriaSnapshot() {
+    await expect(this.headerLocator).toMatchAriaSnapshot();
+  }
+  async categoriesTabHasCorrectAriaSnapshot() {
+    await expect(this.categoriesTabLocator).toMatchAriaSnapshot();
+  }
+  async menuHasCorrectAriaSnapshot() {
+    await expect(this.menuLocator).toMatchAriaSnapshot();
+  }
+  async clozeCookieAlert() {
+    await this.page.getByLabel('Уведомление об использовании cookies').locator('button').click();
+  }
+  async clozeAdvertisingBanner() {
+    await this.page.getByRole('button', { name: 'Закрыть' }).click();
   }
 }
--- tests/specs/unautorized/mainPage.spec.ts
+++ tests/specs/unautorized/mainPage.spec.ts
@@ -5,3 +5,27 @@ test('Открытие главной страницы', async ({ page }) => {
   const mainPage = new MainPage(page);
   await mainPage.open();
 });
+
+test('Проверка доступности элементов хедера', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await mainPage.clozeCookieAlert();
+  await mainPage.clozeAdvertisingBanner();
+  await mainPage.headerHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности табов категорий', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await mainPage.clozeCookieAlert();
+  await mainPage.clozeAdvertisingBanner();
+  await mainPage.categoriesTabHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов бокового меню', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await mainPage.clozeCookieAlert();
+  await mainPage.clozeAdvertisingBanner();
+  await mainPage.menuHasCorrectAriaSnapshot();
+});
